a09675cbfec25c799fe11ad34b395fd2ed50cbf7
Darius Bacon <darius@wry.me>
2014-11-22 09:35:34 -0800

    add compiler (with some loose ends)

--- interpreter.js
+++ interpreter.js
@@ -14,6 +14,9 @@ function mkLit(value) {
         evaluate: function(env, k) {
             return [k, value];
         },
+        compile: function(codeK) {
+            return genContinue(codeK, genRepr(value));
+        },
     };
 }
 
@@ -28,6 +31,9 @@ function mkVar(name) {
                 throw new Error("Unbound variable: " + name);
             return [k, value];
         },
+        compile: function(codeK) {
+            return genContinue(codeK, genName(name));
+        },
     };
 }
 
@@ -38,6 +44,9 @@ function mkCall(receiver, slot) {
         },
         evaluate: function(env, k) {
             return receiver.evaluate(env, [call, slot, k]);
+        },
+        compile: function(codeK) {
+            return receiver.compile(genPushCont('call', genString(slot), codeK));
         },
     };
 }
@@ -63,6 +72,20 @@ function mkExtend(base, name, bindings) {
                 methods[slot] = makeMethod(bindings[slot], name, env);
             return base.evaluate(env, [extendK, methods, k]);
         },
+        compile: function(codeK) {
+            var me = name === null ? '__' : genName(name);
+            var s = '{';
+            var sep = '';
+            for (var slot in bindings) {
+                s += sep + genRepr(slot) + ': function(_, ' + me + ', k) { return ';
+                s += bindings[slot].compile('k');
+                s += '; }';
+                sep = ', ';
+            }
+            s += '}';
+            var codeMethods = s;
+            return base.compile(genPushCont('extendK', codeMethods, codeK));
+        },
     };
 }
 
@@ -82,4 +105,63 @@ function makeSelfishMethod(e, name, env) {
         newEnv[name] = bob;
         return e.evaluate(newEnv, k);
     };
+}
+
+
+// Compiler
+
+function compile(expr, codeK) {
+    if (codeK === undefined) codeK = 'null';
+    return render(expr.compile(codeK));
+}
+
+function render(code) {
+    if (typeof(code) === 'string')
+        return code;
+    else {
+        assert(code.length === 3);
+        return ('[' + render(code[0])
+                + ', ' + render(code[1])
+                + ', ' + render(code[2]) + ']');
+    }
+}
+
+function genContinue(codeK, codeV) {
+    if (typeof(codeK) === 'string')
+        return ('[' + codeK
+                + ', ' + render(codeV) + ']');
+    else {
+        assert(codeK.length === 3);
+        var fn = codeK[0], fv = codeK[1], k = codeK[2];
+        if (fn === 'extendK')
+            return genContinue(k, ('makeBob(' + render(codeV)
+                                   + ', ' + render(fv)));
+        else
+            return render(fn) + ('(' + render(codeV)
+                                 + ', ' + render(fv)
+                                 + ', ' + render(k) + ')');
+    }
+}
+
+function genPushCont(codeFn, codeFreeVar, codeK) {
+    return [codeFn, codeFreeVar, codeK];
+}
+
+function genName(name) {
+    return name + '_b';  // XXX translate non-alphanumeric names to legal JS ones too
+}
+
+function genRepr(value) {
+    if (value === rootBob)
+        return 'rootBob';
+    else if (typeof(value) === 'string')
+        return genString(value);
+    else if (typeof(value) === 'boolean' || typeof(value) === 'number')
+        return ''+value;
+    else
+        throw new Error("Unknown literal type: " + value);
+}
+
+function genString(s) {
+    return '"' + s + '"'; // XXX escaping
 }
